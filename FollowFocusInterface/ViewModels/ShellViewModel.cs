0a0629e9279e4aa7b6b0d33c4421e17fdc551d24
Vid Cresnik <vcresnik@gmail.com>
2020-07-18 17:36:24 +0200

    Adds basic functionality of a Window (minimize, maximize, close, drag,...)

--- FollowFocusInterface/ViewModels/ShellViewModel.cs
+++ FollowFocusInterface/ViewModels/ShellViewModel.cs
@@ -1,9 +1,54 @@
 ﻿using Caliburn.Micro;
+using System.Windows;
 
 namespace FollowFocusInterface.ViewModels
 {
     public class ShellViewModel : Screen
     {
+
+        #region Window Control
+
+        private WindowState windowState;
+        public WindowState WindowState
+        {
+            get { return windowState; }
+            set
+            {
+                windowState = value;
+                NotifyOfPropertyChange(() => WindowState);
+            }
+        }
+
+        public void MaximizeWindow()
+        {
+            if (WindowState == WindowState.Maximized)
+                WindowState = WindowState.Normal;
+            else
+                WindowState = WindowState.Maximized;
+        }
+
+        public void MinimizeWindow()
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        public bool myCondition { get { return (false); } }
+
+        public void CloseWindow()
+        {
+            Application.Current.Shutdown();
+        }
+
+        #endregion
+
+        #region Constructor
+
+        public ShellViewModel()
+        {
+
+        }
+
+        #endregion
 
     }
 }
